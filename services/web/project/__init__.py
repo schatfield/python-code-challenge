8ecc836c7f8af287edbdc85e713158410e5c5c38
Schatfield <dgoldmine@icloud.com>
2020-09-23 17:12:27 -0500

    made changes to requirements.txt file, created additional files for possible refactoring use, added notes in to init.py, and installed Marshmallow to start on schemas

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,9 +1,31 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 
+# INIT APP
 app = Flask(__name__)
 
+# DATABASE?
+app.config.from_object("project.config.Config")
 
-@app.route("/")
-def hello_world():
-    return jsonify(hello="world")
+# INIT DB
+db = SQLAlchemy(app)
+
+#INIT MARSHMALLOW
+ma = Marshmallow(app)
+
+
+# CLASS/MODEL
+class BudgetItem(db.Model):
+    __tablename__ = "budget item"
+
+    id = db.Column(db.Integer, primary_key=True)
+    item_name= db.Column(db.String(128), unique=True, nullable=False)
+    item_cost= db.Column(db.Integer, unique=True, nullable=False)
+    total_budget= db.Column(db.Integer, unique=True, nullable=False)
+    available_budget= db.Column(db.Integer, unique=True, nullable=False)
+
+    # def __init__(self, email):
+    #     self.email = email
+
